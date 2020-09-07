589db8ab38958603e663cc142ec704517f46dec2
Jade Scarmoncin <jade.scarmoncin@everycheck.fr>
2020-09-07 11:14:24 +0200

    delete et post rajoutés

--- main.go
+++ main.go
@@ -1,65 +1,117 @@
 package main
 
 import (
-	//"encoding/json"
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
 	"net/http"
 	//"reflect"
+	"os"
+	"strconv"
 	"time"
 )
 
 type User struct {
+	id   int
 	mail string
 }
 
 type Message struct {
+	id   int
 	user User
 	date time.Time
 }
 
 type Discussion struct {
+	id    int
 	sujet string
 	mess  []Message
 }
 
-var users = make([]User, 100)
+var users = make([]User, 0, 20)
+var userCount = 0
+
+func appendUser(email string) {
+	userCount++
+	users = append(users, User{userCount, email})
+}
+
+func initData() {
+	appendUser("test1@example.com")
+	appendUser("test2@example.com")
+	appendUser("test3@example.com")
+}
 
 func main() {
+	initData()
+	fmt.Println(users[0])
 	fmt.Println("Début du forum")
-	users[0] = User{"jeSuisUnUser@gmail.com"}
-	users[1] = User{"jeSuisUnAutreUser@gmail.com"}
-	users[2] = User{"jeSuisLeDernierUser@gmail.com"}
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
 	r.Route("/users", func(r chi.Router) {
-		//r.Get("/{mail}", requestSay)
+		r.Get("/{id}", requestSay)
 		r.Get("/", requestSayAll)
+		r.Post("/", createUser)
+		r.Put("/{id}", updateData)
+		r.Delete("/{id}", deleteUser)
 	})
 	http.ListenAndServe(":8080", r)
 }
 func requestSayAll(w http.ResponseWriter, r *http.Request) {
-	/*e, err := json.MarshalIndent(users, "", "  ")
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Println(err)
-		return
+	for user := range users {
+		e, err := json.MarshalIndent(users[user].mail, "", "  ")
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Println(err)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintln(w, string(e))
 	}
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprintln(w, string(e))*/
-	w.Write([]byte("hello"))
 }
 
-/*func requestSay(w http.ResponseWriter, r *http.Request) {
-	id := chi.URLParam(r, "mail")
-	value, ok :=
-	if ok {
+func requestSay(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	indice, err := strconv.Atoi(id)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(2)
+	}
+	if (indice - 1) < len(users) {
+		//if users[indice-1].mail != "" {
+		e, err := json.MarshalIndent(users[indice-1].mail, "", "  ")
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Println(err)
+			return
+		}
 		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(value))
+		fmt.Fprintln(w, string(e))
 		return
 	}
 	w.WriteHeader(http.StatusNotFound)
-	w.Write([]byte("Cette data n'existe pas"))
-} */
+	w.Write([]byte("Cet utilisateur n'existe pas"))
+}
+
+func createUser(w http.ResponseWriter, r *http.Request) {
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(r.Body)
+	userCount++
+	appendUser(buf.String())
+	w.WriteHeader(http.StatusCreated)
+}
+
+func deleteUser(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	indice, err := strconv.Atoi(id)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(2)
+	}
+	users = append(users[:(indice-1)], users[indice:]...)
+	userCount--
+	w.WriteHeader(http.StatusNoContent)
+}
