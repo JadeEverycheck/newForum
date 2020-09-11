8493b609376a18fd2dc553ffac382f500a564c5f
Jade Scarmoncin <jade.scarmoncin@everycheck.fr>
2020-09-11 17:44:43 +0200

    add cors

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module forum
 
 go 1.14
 
-require github.com/go-chi/chi v4.1.2+incompatible
+require (
+	github.com/go-chi/chi v4.1.2+incompatible
+	github.com/go-chi/cors v1.1.1 // indirect
+)
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"forum/api"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/cors"
 	"net/http"
 )
 
@@ -14,6 +15,16 @@ func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
+	r.Use(cors.Handler(cors.Options{
+		// AllowedOrigins: []string{"https://foo.com"}, // Use this to allow specific origin hosts
+		AllowedOrigins: []string{"*"},
+		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
 	r.Route("/users", func(r chi.Router) {
 		r.Get("/{id}", api.GetUser)
 		r.Get("/", api.GetAllUser)
