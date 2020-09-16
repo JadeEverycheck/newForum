1973a543126fbd37b94c1b835b6f0b2607216020
JadeEverycheck <70909237+JadeEverycheck@users.noreply.github.com>
2020-09-16 16:58:36 +0200

    Change hostname

--- static/js/host.js
+++ static/js/host.js
@@ -2,7 +2,7 @@
 let host = localStorage.getItem("host")
 
 if (host == null) {
-	localStorage.setItem('host', "https://protected-brushlands-86376.herokuapp.com")
+	localStorage.setItem('host', "https://boiling-waters-46327.herokuapp.com")
 	// localStorage.setItem('host', "http://localhost:8080")
 	host = localStorage.getItem("host")
 }
